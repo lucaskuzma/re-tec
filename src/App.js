4250771a2c36383b3d7a3bd16ddb4c55eaebce01
lucas <lucas@thestrangeagency.com>
2022-02-19 13:28:48 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props)
+    let value = 'a - b 51, dog 3\nb - dog 2\nc - a 5\ndog - c 3'
+    this.state = {
+      // scrollTop: 0,
+      ...App.updateResult(value),
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this);
+    // this.copyToClipboard = this.copyToClipboard.bind(this);
+    // this.handleScroll = this.handleScroll.bind(this);
+    // this.createMarkup = this.createMarkup.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState(
+      App.updateResult(event.target.value)
+    )
+  }
+
+  static parse(line) {
+    const regex = /(\w+)\s*-\s*(.*)/
+    const match = regex.exec(line);
+    let out = 'empty\n'
+    if (match) {
+      const name = match[1]
+      const connections = match[2]
+      
+      let connectionString = ''
+      for (const connection of connections.split(',')) {
+        const trimmed = connection.trim()
+        const regex = /(\w+)\s*(\d+)/
+        const match = regex.exec(connection);
+        const destination = match[1]
+        const length = match[2]
+        connectionString += `[${destination} : ${length}] `
+      }
+
+      out = `${name} ➤ ${connectionString}\n`
+      console.log(match)
+    }
+    return out
+  }
+
+  static updateResult(text) {
+    let lineCount = 1; // rough estimate of lines + line wraps to calculate entry height
+
+    let lines = text.split('\n');
+    let output = ''
+    for(let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+
+      lineCount += 1 + Math.floor(line.length / 38);
+      
+      output += this.parse(line)
+    }
+    // let output = text;
+
+    return {
+      value: text,
+      output: output,
+      rows: lineCount,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="center">
+          <form>
+            <textarea
+              ref="entry"
+              className="App-entryArea App-textArea"
+              rows={this.state.rows}
+              type="text"
+              value={this.state.value}
+              // rows={40}
+              onScroll={this.handleScroll}
+              onChange={this.handleChange}
+            />
+            <textarea
+              ref="output"
+              className="App-outputArea App-textArea"
+              rows={this.state.rows}
+              type="text"
+              // rows={40}
+              value={this.state.output}
+              onScroll={this.handleScroll}
+              readOnly
+            />
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
